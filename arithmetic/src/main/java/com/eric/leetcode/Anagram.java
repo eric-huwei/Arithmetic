4b0bec0f46b5748387c4cc2649bba925005d78dc
HUWEI <scorpio.huwei@gmail.com>
2020-08-03 23:12:13 +0800

    有效的字母异位词

---
+++ arithmetic/src/main/java/com/eric/leetcode/Anagram.java
@@ -0,0 +1,46 @@
+package com.eric.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @DESCIRPTION 有效的字母异位词
+ * @AUTHOR SCORPIO.HU
+ * @DATE 2020/8/3 下午9:22
+ */
+public class Anagram {
+
+    public static boolean isAnagram(String s, String t) {
+        char[] strS = s.toCharArray();
+        char[] strT = t.toCharArray();
+        Arrays.sort(strS);
+        Arrays.sort(strT);
+
+        return Arrays.equals(strS, strT);
+    }
+
+    public static boolean isAnagramHash(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+        String s = "anagram", t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+}
