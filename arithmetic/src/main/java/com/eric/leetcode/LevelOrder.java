6e0ed18714e1ef826b471f8807de2e1f9f17709a
HUWEI <scorpio.huwei@gmail.com>
2020-08-31 20:41:26 +0800

    二叉树层序遍历  -- 递归

---
+++ arithmetic/src/main/java/com/eric/leetcode/LevelOrder.java
@@ -0,0 +1,41 @@
+package com.eric.leetcode;
+
+import com.eric.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @DESCIRPTION 二叉树的层序遍历
+ * @AUTHOR SCORPIO.HU
+ * @DATE 2020/8/31 上午12:55
+ */
+public class LevelOrder {
+
+    List<List<Integer>> levels = new ArrayList<List<Integer>>();
+
+    //递归
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (null == root) {
+            return levels;
+        }
+
+        recursion(root, 0);
+        return levels;
+    }
+
+    private void recursion(TreeNode node, int level) {
+        if (levels.size() == level) {
+            levels.add(new ArrayList<Integer>());
+        }
+
+        levels.get(level).add(node.val);
+
+        if (null != node.left) {
+            recursion(node.left, level + 1);
+        }
+        if (null != node.right) {
+            recursion(node.right, level + 1);
+        }
+    }
+}
